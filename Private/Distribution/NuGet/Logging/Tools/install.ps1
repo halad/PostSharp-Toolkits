c387cb9ce767c44b69a85ca2f659ccb4469c0661
SharpCrafters <info@sharpcrafters.com>
2012-02-15 15:36:57 +0100

    Moved and separated NuGet build scripts into two packages. Fixed Release build

---
+++ Private/Distribution/NuGet/Logging/Tools/install.ps1
@@ -0,0 +1,26 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+
+# Set the psproj name to be the Project's name, i.e. 'ConsoleApplication1.psproj'
+$psprojectName = $project.Name + ".psproj"
+
+# Find the Template file as placed by the NuGet script
+$psproj = $project.ProjectItems.Item("Template.psproj");
+
+# Set the new filename to the Template
+$psproj.Name = $psprojectName;
+
+$psprojectFile = $path::ChangeExtension($project.FileName, ".psproj")
+$weaverFile = $path::Combine($toolsPath, "PostSharp.Toolkit.Instrumentation.Weaver.dll");
+
+# Make the path to the targets file relative.
+$projectUri = new-object Uri('file://' + $psprojectFile)
+$targetUri = new-object Uri('file://' + $weaverFile)
+$relativePath = $projectUri.MakeRelativeUri($targetUri).ToString().Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+
+$xml = [xml](Get-Content $psprojectFile)
+$element = $xml.CreateElement("Using", "http://schemas.postsharp.org/1.0/configuration");
+$element.SetAttribute("File", $relativePath);
+$xml.Project.AppendChild($element);
+$xml.Save($psprojectFile)
