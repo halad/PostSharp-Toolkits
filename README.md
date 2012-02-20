b0bf8ac7c2de3b3630ea7335fd23d6de1438b0df
Igal Tabachnik <igal@sharpcrafters.com>
2012-02-20 16:20:00 +0100

    Tidy up some AssemblyInfo information. Version changed to 0.0 temporary everywhere

---
+++ README.md
@@ -0,0 +1,9 @@
+﻿PostSharp Toolkits
+==================
+
+Welcome to PostSharp Toolkits!
+
+What is PostSharp Toolkits?
+---------------------------
+
+PostSharp Toolkits is a collection of toolkits ...
---
+++ SharedAssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+[assembly: AssemblyCompany("SharpCrafters s.r.o.")]
+[assembly: AssemblyProduct("PostSharp Toolkits")]
+[assembly: AssemblyCopyright("Copyright © 2012 SharpCrafters s.r.o. ")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
