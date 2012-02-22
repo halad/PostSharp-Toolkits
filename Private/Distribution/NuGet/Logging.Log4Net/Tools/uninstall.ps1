79d20083e2a8e050171b3b00d242dfa12049a744
Igal Tabachnik <igal@sharpcrafters.com>
2012-02-22 19:47:57 +0100

    Adding the Log4Net Toolkit NuGet files

---
+++ Private/Distribution/NuGet/Logging.Log4Net/Tools/uninstall.ps1
@@ -0,0 +1,30 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$path = [System.IO.Path]
+
+# Set the psproj name to be the Project's name, i.e. 'ConsoleApplication1.psproj'
+$psprojectName = $project.Name + ".psproj"
+
+# Check if the file previously existed in the project
+$psproj = $project.ProjectItems | where { $_.Name -eq $psprojectName }
+if (!$psproj)
+{
+  return
+}
+
+$psprojectFile = $psproj.Properties.Item("FullPath").Value
+$xml = [xml](Get-Content $psprojectFile)
+
+# Remove the logging backend property
+$xml.Project.Property | where { $_.Name -eq 'LoggingBackend' } | foreach {
+  $_.ParentNode.RemoveChild($_)
+  Write-Output "Removed property LoggingBackend"
+}
+
+# Remove the weaver
+$xml.Project.Using | where { $_.File -like '*PostSharp.Toolkit.Diagnostics.Weaver.Log4Net.dll*' } | foreach {
+  $_.ParentNode.RemoveChild($_)
+  Write-Output "Removed the NLog weaver"
+}
+
+$xml.Save($psprojectFile)
