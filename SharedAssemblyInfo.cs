0fe1fc9df906970712c60acb97d9b7be12b03f4d
Igal Tabachnik <igal@sharpcrafters.com>
2012-02-23 14:35:46 +0100

    Added a versioning script to create proper build number, currently based on PostSharp's build. Updated NuGet packages to receive the build number.

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -1,8 +1,9 @@
 using System.Reflection;
+using System.Resources;
 
 [assembly: AssemblyCompany("SharpCrafters s.r.o.")]
-[assembly: AssemblyProduct("PostSharp Toolkits")]
-[assembly: AssemblyCopyright("Copyright © 2012 SharpCrafters s.r.o. ")]
-
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyProduct("PostSharp Toolkits 2.1")]
+[assembly: AssemblyCopyright("Copyright (c) 2012 by SharpCrafters s.r.o.")]
+[assembly: AssemblyTrademark("PostSharp is a registered trademark of SharpCrafters s.r.o.")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en-US")]
