c0b255085acc917c89e94ba660298d27033b5cdd
Igal Tabachnik <igal@sharpcrafters.com>
2012-03-01 09:59:44 +0100

    Added more test cases for TraceTests

--- Tests/PostSharp.Toolkit.Tests.Trace/TraceTests.cs
+++ Tests/PostSharp.Toolkit.Tests.Trace/TraceTests.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 using TestAssembly;
 
@@ -19,12 +16,46 @@ namespace PostSharp.Toolkit.Tests.Trace
         }
 
         [Test]
-        public void UnderTest_Scenario_ExpectedResult()
+        public void Trace_Methods_LogsMethodEnterAndExit()
         {
             SimpleClass s = new SimpleClass();
             s.Method1();
 
-            string output = OutputString.ToString().Trim();
+            string output = OutputString.ToString();
+            StringAssert.Contains("Entering: TestAssembly.SimpleClass/Method1() : void (None)", output);
+            StringAssert.Contains("Exiting: TestAssembly.SimpleClass/Method1() : void (None)", output);
+        }
+
+        [Test]
+        public void Trace_Properties_LogsPropertyGetter()
+        {
+            SimpleClass s = new SimpleClass();
+            string value = s.Property1;
+
+            string output = OutputString.ToString();
+            StringAssert.Contains("Entering: TestAssembly.SimpleClass/get_Property1() : string (None)", output);
+            StringAssert.Contains("Exiting: TestAssembly.SimpleClass/get_Property1() : string (None)", output);
+        }
+
+        [Test]
+        public void Trace_Properties_LogsPropertySetter()
+        {
+            SimpleClass s = new SimpleClass();
+            s.Property1 = "Test";
+
+            string output = OutputString.ToString();
+            StringAssert.Contains("Entering: TestAssembly.SimpleClass/set_Property1(string value) : void (None)", output);
+            StringAssert.Contains("Exiting: TestAssembly.SimpleClass/set_Property1(string value) : void (None)", output);
+        }
+
+        [Test]
+        public void Trace_SimpleClassWithFields_LoggingNotAppliedToField()
+        {
+            SimpleClass s = new SimpleClass();
+            s.Field1 = "Test";
+
+            string output = OutputString.ToString();
+            StringAssert.DoesNotContain("Field1", output);
         }
     }
 }
